2ff71db2cd73d63301a5ebf064079255c3e4476c
Gilles Fabre <gillesfabre34@gmail.com>
2021-08-12 12:19:13 +0200

    Create files by kata

--- backend/src/services/solutions-files.service.ts
+++ backend/src/services/solutions-files.service.ts
@@ -4,32 +4,31 @@ import { CONFIG } from '../const/config';
 import * as chalk from 'chalk';
 import { ensureDir, existsSync, writeFile } from '../utils/file-system.util';
 import { cleanHtml } from '../utils/html.util';
+import { SolutionEntity } from '../entities/solution.entity';
 
 export class SolutionsFilesService {
 
     static async createFiles(): Promise<void> {
         await ensureDir(`${CONFIG.root}/dist/solutions`);
         const kataLanguageEntities: KataLanguageEntity[] = await KataLanguageEntityService.findAllKataLanguage(CONFIG.language);
-        console.log(chalk.magentaBright('KLE LGTHHHH'), kataLanguageEntities?.length);
+        // console.log(chalk.magentaBright('KLE LGTHHHH'), kataLanguageEntities?.length);
         for (const kle of kataLanguageEntities) {
-            console.log(chalk.magentaBright('KLE IDDDD'), kle.path);
-            console.log(chalk.cyanBright('KLE SOLSSSS'), kle.solutionEntities.length);
-
-            // console.log(chalk.magentaBright('KLE CODDDDD'), this.getOutsideNodes(kle));
-            await writeFile(kle.path, this.getCode(kle));
-            // if (!existsSync(kle.path)) {
-            //     await this.createFile(kle);
-            // }
+            // console.log(chalk.magentaBright('KLE IDDDD'), kle.path);
+            // console.log(chalk.cyanBright('KLE SOLSSSS'), kle.solutionEntities.length);
+            await this.createFilesByKataLanguage(kle);
         }
     }
 
-    static async createFile(kle: KataLanguageEntity): Promise<void> {
-        console.log(chalk.cyanBright('KLE IDDDD'), kle.id);
-
+    private static async createFilesByKataLanguage(kle: KataLanguageEntity): Promise<void> {
+        for (const solutionEntity of kle.solutionEntities) {
+            // console.log(chalk.cyanBright('KLE IDDDD'), solutionEntity.code);
+            const path: string = `${kle.path}/${solutionEntity.id}.ts`;
+            await writeFile(path, this.getCode(solutionEntity));
+        }
     }
 
-    static getCode(kle: KataLanguageEntity): string {
-        const code: string = kle.solutionEntities.map(s => cleanHtml(s.code)).join('\n\n');
+    private static getCode(solutionEntity: SolutionEntity): string {
+        const code: string = cleanHtml(solutionEntity.code);
         return `${code} \n`;
     }
 }
